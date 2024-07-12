c361040e29d860004f3b5072966e1313ded646e6
Daniel Buchan <clone@myself.com>
2024-07-12 17:54:47 +0100

    corrected bug in merizo search output with header

--- merizo_search/programs/utils.py
+++ merizo_search/programs/utils.py
@@ -29,63 +29,84 @@ def check_for_database(db_name):
         sys.exit(1)
 
 def write_search_results(results: list[dict], output_file: str, format_list: str, header: bool):
-    
+        
     with open(output_file, 'w+') as fn:
-        for res in results:
-            for k, match in res.items():
-                formatted_output = []
-                head_str=''
+        if header: 
+            head_str=''
+            for option in format_list:
                 for option in format_list:
                     if option == 'query':
                         head_str+='query\t'
-                        formatted_output.append(match['query'])
                     elif option == 'target':
                         head_str+='target\t'
-                        formatted_output.append(match['target'])
                     elif option == 'chopping':
                         head_str+='dom_str\t'
-                        formatted_output.append(match['dom_str'])
                     elif option == 'conf':
                         head_str+='dom_conf\t'
-                        formatted_output.append("{:.4f}".format(match['dom_conf']))
                     elif option == 'plddt':
                         head_str+='dom_plddt\t'
-                        formatted_output.append("{:.4f}".format(match['dom_plddt']))
                     elif option == 'emb_rank':
                         head_str+='emb_rank\t'
-                        formatted_output.append("{}".format(k))
                     elif option == 'emb_score':
                         head_str+='emb_score\t'
-                        formatted_output.append("{:.4f}".format(match['score']))
                     elif option == 'q_len':
                         head_str+='q_len\t'
-                        formatted_output.append("{}".format(match['q_len']))
                     elif option == 't_len':
                         head_str+='t_len\t'
-                        formatted_output.append("{}".format(match['t_len']))
                     elif option == 'ali_len':
                         head_str+='ali_len\t'
-                        formatted_output.append("{}".format(match['tmalign_output']['len_ali']))
                     elif option == 'seq_id':
                         head_str+='seq_id\t'
-                        formatted_output.append("{:.4f}".format(match['tmalign_output']['seq_id']))
                     elif option == 'q_tm':
                         head_str+='q_tm\t'
-                        formatted_output.append("{:.4f}".format(match['tmalign_output']['qtm']))
                     elif option == 't_tm':
                         head_str+='t_tm\t'
-                        formatted_output.append("{:.4f}".format(match['tmalign_output']['ttm']))
                     elif option == 'max_tm':
                         head_str+='max_t\t'
-                        formatted_output.append("{:.4f}".format(max(match['tmalign_output']['qtm'], match['tmalign_output']['ttm'])))
                     elif option == 'rmsd':
                         head_str+='rmsd\t'
-                        formatted_output.append("{:.2f}".format(match['tmalign_output']['rmsd']))
                     else:
                         logger.warning(f"Format option '{option}' is not recognized.")
                         sys.exit(1)
-                if header:
-                    fn.write(f'{head_str.rstrip()}\n')     
+            fn.write(f'{head_str.rstrip()}\n')
+            
+        for res in results:
+            for k, match in res.items():
+                formatted_output = []
+                for option in format_list:
+                    if option == 'query':
+                        formatted_output.append(match['query'])
+                    elif option == 'target':
+                        formatted_output.append(match['target'])
+                    elif option == 'chopping':
+                        formatted_output.append(match['dom_str'])
+                    elif option == 'conf':
+                        formatted_output.append("{:.4f}".format(match['dom_conf']))
+                    elif option == 'plddt':
+                        formatted_output.append("{:.4f}".format(match['dom_plddt']))
+                    elif option == 'emb_rank':
+                        formatted_output.append("{}".format(k))
+                    elif option == 'emb_score':
+                        formatted_output.append("{:.4f}".format(match['score']))
+                    elif option == 'q_len':
+                        formatted_output.append("{}".format(match['q_len']))
+                    elif option == 't_len':
+                        formatted_output.append("{}".format(match['t_len']))
+                    elif option == 'ali_len':
+                        formatted_output.append("{}".format(match['tmalign_output']['len_ali']))
+                    elif option == 'seq_id':
+                        formatted_output.append("{:.4f}".format(match['tmalign_output']['seq_id']))
+                    elif option == 'q_tm':
+                        formatted_output.append("{:.4f}".format(match['tmalign_output']['qtm']))
+                    elif option == 't_tm':
+                        formatted_output.append("{:.4f}".format(match['tmalign_output']['ttm']))
+                    elif option == 'max_tm':
+                        formatted_output.append("{:.4f}".format(max(match['tmalign_output']['qtm'], match['tmalign_output']['ttm'])))
+                    elif option == 'rmsd':
+                        formatted_output.append("{:.2f}".format(match['tmalign_output']['rmsd']))
+                    else:
+                        logger.warning(f"Format option '{option}' is not recognized.")
+                        sys.exit(1)  
                 fn.write('\t'.join(formatted_output) + '\n')
                 
 def write_segment_results(results: list[dict], output_file: str, header: bool):
