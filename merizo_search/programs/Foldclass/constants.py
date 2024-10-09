fff0487f21a1a6c28146de8a83e6bb1c8ccec41d
Daniel <clone@myself.com>
2024-10-09 12:29:03 +0100

    Update constants.py

--- merizo_search/programs/Foldclass/constants.py
+++ merizo_search/programs/Foldclass/constants.py
@@ -2,7 +2,8 @@ three_to_single_aa = {
     'ALA': 'A', 'CYS': 'C', 'ASP': 'D', 'GLU': 'E', 'PHE': 'F',
     'GLY': 'G', 'HIS': 'H', 'ILE': 'I', 'LYS': 'K', 'LEU': 'L',
     'MET': 'M', 'ASN': 'N', 'PRO': 'P', 'GLN': 'Q', 'ARG': 'R',
-    'SER': 'S', 'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y'
+    'SER': 'S', 'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y',
+    'UNK': 'X',
 }
 
 single_to_three_aa = {v: k for k, v in three_to_single_aa.items()}
