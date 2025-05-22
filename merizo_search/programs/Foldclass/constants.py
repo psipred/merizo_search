ce6ae117361fdc0c17469351ec9c55cf58b7c234
Daniel Buchan <clone@myself.com>
2025-05-22 17:04:46 +0100

    updated support for amino acid TLC, for valid ATOM different protonation states for 4 amino acids

--- merizo_search/programs/Foldclass/constants.py
+++ merizo_search/programs/Foldclass/constants.py
@@ -3,7 +3,8 @@ three_to_single_aa = {
     'GLY': 'G', 'HIS': 'H', 'ILE': 'I', 'LYS': 'K', 'LEU': 'L',
     'MET': 'M', 'ASN': 'N', 'PRO': 'P', 'GLN': 'Q', 'ARG': 'R',
     'SER': 'S', 'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y',
-    'UNK': 'X',
+    'UNK': 'X', 'ASH': 'D', 'GLH': 'E', 'HID': 'H', 'HIE': 'H',
+    'HIP': 'H', 'HSD': 'H', 'HSE': 'H', 'LYN': 'K',
 }
 
 single_to_three_aa = {v: k for k, v in three_to_single_aa.items()}
